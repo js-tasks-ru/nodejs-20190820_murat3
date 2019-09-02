a10038cc848e16ae9ff6ef8c9b7e11c93ccf0137
Murat <murat351@narod.ru>
2019-09-02 06:53:32 +0300

    soil 2- 1

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -3,11 +3,24 @@ const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
-    super(options);
+	super();
+	this.options = options.limit;
+	this.knt = 0; 
   }
+  fff(chunk){
+  	//console.log ('chunk');
+    this.knt=this.knt+chunk.length;
+    if(this.knt>this.options){
+    //	console.log ('limit');
+    	throw new LimitExceededError();
+   }
+
+  	return chunk;
+    } 
 
   _transform(chunk, encoding, callback) {
-
+    this.push(this.fff(chunk));
+    callback(null);
   }
 }
 
