ecd1301ecf2a678ead11e8afc875f72eda9256ae
Murat <murat351@narod.ru>
2019-09-04 22:36:04 +0300

    add m2 n1

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -6,22 +6,25 @@ class LimitSizeStream extends stream.Transform {
 	super();
 	this.options = options.limit;
 	this.knt = 0; 
+  this.cb=null;
   }
   fff(chunk){
-  	//console.log ('chunk');
+    //console.log ('chunk');
     this.knt=this.knt+chunk.length;
     if(this.knt>this.options){
-    //	console.log ('limit');
-    	throw new LimitExceededError();
-   }
+      //  console.log ('limit');
+      this.cb= new LimitExceededError();
+          return null;
+    }
+    return chunk;
+  }
 
-  	return chunk;
-    } 
 
   _transform(chunk, encoding, callback) {
     this.push(this.fff(chunk));
-    callback(null);
+        callback(this.cb);
+
   }
-}
+}   
 
 module.exports = LimitSizeStream;
